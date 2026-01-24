9f526725071afa96ce709d2d7efc0d9811b4c392
yoga12 <chohaeinjoan@gmail.com>
2026-01-24 17:18:28 +0900

    지출 팝업창 수정

--- app/src/main/java/OutgoingSetting.kt
+++ app/src/main/java/OutgoingSetting.kt
@@ -16,7 +16,7 @@ class OutgoingSetting : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_outgoingSetting)
+        setContentView(R.layout.activity_outgoingsetting)
 
         initView()
         setListeners()
