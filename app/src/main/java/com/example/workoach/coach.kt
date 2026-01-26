aa311cfe1544d0ea948194b18a99e44b4b01c8c2
sayhyun20051007-cyber <sayhyun20051007@gmail.com>
2026-01-26 18:35:13 +0900

    홈페이지 빼빼로 이미지 수정, 코칭페이지 만듦

--- app/src/main/java/com/example/workoach/coach.kt
+++ app/src/main/java/com/example/workoach/coach.kt
@@ -2,16 +2,13 @@ package com.example.workoach
 
 import android.os.Bundle
 import android.widget.ProgressBar
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class testttttttttttt : AppCompatActivity() {
+class coach : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_testttttttttttt)
+        setContentView(R.layout.activity_coach)
 
         val moneyBar = findViewById<ProgressBar>(R.id.moneyBar)
 
